c00e5a3e87571c4750eb2fb42c2b0d5768a9a416
Daniel Carvalho <danieljosecarvalho@gmail.com>
2016-07-07 20:52:30 -0300

    - Classe DatabaseHelper; - Classe Usuario; - Classe PersonalHealthDAO;

---
+++ app/src/main/java/com/carvalhodj/personalhealth/dao/Dev01PersonalHealthDAO.java
@@ -0,0 +1,106 @@
+package com.carvalhodj.personalhealth.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.carvalhodj.personalhealth.dominio.Dev00Usuario;
+import com.carvalhodj.personalhealth.infra.Dev00DatabaseHelper;
+
+public class Dev01PersonalHealthDAO {
+    private Dev00DatabaseHelper helper;
+
+    public Dev01PersonalHealthDAO(Context context){
+        helper = new Dev00DatabaseHelper(context);
+    }
+
+    public Dev00Usuario getUsuario(String email) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String comando = "SELECT * FROM " + Dev00DatabaseHelper.TABLE_USER +
+                " WHERE " + Dev00DatabaseHelper.COLUMN_EMAIL + " LIKE ?";
+
+        String[] argumentos = {email};
+
+        Cursor cursor = db.rawQuery(comando, argumentos);
+
+        Dev00Usuario usuario = null;
+
+        if (cursor.moveToNext()) {
+
+            String idColumn= Dev00DatabaseHelper.COLUMN_ID;
+            int indexColumnID= cursor.getColumnIndex(idColumn);
+            long id = cursor.getLong(indexColumnID);
+
+            String senhaColumn= Dev00DatabaseHelper.COLUMN_PASS;
+            int indexColumnSenha= cursor.getColumnIndex(senhaColumn);
+            String senha = cursor.getString(indexColumnSenha);
+
+            usuario = new Dev00Usuario();
+            usuario.setId(id);
+            usuario.setEmail(email);
+            usuario.setPass(senha);
+        }
+        cursor.close();
+        db.close();
+
+        return usuario;
+    }
+
+    public Dev00Usuario getUsuario(String email, String senha){
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String comando = "SELECT * FROM " + Dev00DatabaseHelper.TABLE_USER +
+                " WHERE " + Dev00DatabaseHelper.COLUMN_EMAIL + " LIKE ? AND " +
+                Dev00DatabaseHelper.COLUMN_PASS + " LIKE ?";
+
+        String[] argumentos = {email, senha};
+
+        Cursor cursor = db.rawQuery(comando, argumentos);
+
+        Dev00Usuario usuario = null;
+
+        if (cursor.moveToNext()) {
+
+            String idColumn= Dev00DatabaseHelper.COLUMN_ID;
+            int indexColumnID= cursor.getColumnIndex(idColumn);
+            long id = cursor.getLong(indexColumnID);
+
+            usuario = new Dev00Usuario();
+            usuario.setId(id);
+            usuario.setEmail(email);
+            usuario.setPass(senha);
+        }
+        cursor.close();
+        db.close();
+
+        return usuario;
+    }
+
+    public long inserir(Dev00Usuario usuario) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        String nomeColumn = Dev00DatabaseHelper.COLUMN_NAME;
+        String nome = usuario.getNome();
+
+        String emailColumn = Dev00DatabaseHelper.COLUMN_EMAIL;
+        String email = usuario.getEmail();
+
+        String senhaColumn = Dev00DatabaseHelper.COLUMN_PASS;
+        String senha = usuario.getPass();
+
+        values.put(nomeColumn, nome);
+        values.put(emailColumn, email);
+        values.put(senhaColumn, senha);
+
+        String tabela = Dev00DatabaseHelper.TABLE_USER;
+
+        long id = db.insert(tabela, null, values);
+
+        db.close();
+        return id;
+    }
+}
---
+++ app/src/main/java/com/carvalhodj/personalhealth/dominio/Dev00Usuario.java
@@ -0,0 +1,40 @@
+package com.carvalhodj.personalhealth.dominio;
+
+public class Dev00Usuario {
+    private String nome;
+    private String email;
+    private String pass;
+    private long id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
