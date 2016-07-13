d02f958468d5726e9b7e15f19c350a0cc855757e
carvalhodj <danieljosecarvalho@gmail.com>
2016-07-13 14:51:14 -0300

    - Logomarca; - Tela login sem action bar;

--- app/src/main/java/com/carvalhodj/personalhealth/gui/LoginActivity.java
+++ app/src/main/java/com/carvalhodj/personalhealth/gui/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.carvalhodj.personalhealth.gui;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 import com.carvalhodj.personalhealth.R;
@@ -24,6 +27,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         sessao.reset();
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.backgroundPersonalHealthDark));
     }
 
     public void onClickLogin(View v){
